8baeb115c238922af81fa5c7ae2a2fc96b2b8791
elhombretecla <delacruzgarciajuan@gmail.com>
2014-07-17 11:43:17 +0200

    talks view added + talks menu added

--- js/features.js
+++ js/features.js
@@ -9,4 +9,8 @@ jQuery(document).ready(function() {
         $('nav.event-mobile').slideToggle("fast");
         $('body').removeClass('translate-3d-menu');
     });
+    $('#talks-nav-icon').click(function() {
+        $('nav.talks-mobile').slideToggle("fast");
+        $('body').toggleClass('translate-3d-menu-left');
+    });
 });
